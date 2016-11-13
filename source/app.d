594fd16af0c67d4ab330328134f2d8caffeb51df
Andrea Orru <andreaorru1991@gmail.com>
2016-11-13 18:28:50 -0500

    Writing everything from scratch.

--- source/app.d
+++ source/app.d
@@ -1,16 +1,7 @@
-import xlang.compiler;
-import xlang.vm;
-import std.algorithm;
 import std.stdio;
 
 
 void main()
 {
-    writeln(">>> Enter code to evaluate. Press Ctrl+D to finish inserting.");
-
-    string expression;
-    readf("%s", &expression);
-
-    writeln("\n>>> Result of the evaluation:");
-    writeln(expression.bytecode.run());
+    writeln("Hello world!");
 }
